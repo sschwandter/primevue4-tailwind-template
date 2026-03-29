4bf1119c992e9fd9c7e32b3410701995ad09d967
Stefan Schwandter <s.schwandter@me.com>
2026-03-29 09:18:36 +0200

    Switch app preset to Lara

--- src/theme/preset.ts
+++ src/theme/preset.ts
@@ -1,7 +1,7 @@
 import { definePreset } from "@primeuix/themes";
-import Aura from "@primeuix/themes/aura";
+import Lara from "@primeuix/themes/lara";
 
-// Define a custom preset based on the Aura theme - if needed, you can customize it by passing options in the second argument
-const AppPreset = definePreset(Aura, {});
+// Define a custom preset based on the given PrimeVue theme - if needed, you can customize it by passing options in the second argument
+const AppPreset = definePreset(Lara, {});
 
 export default AppPreset;
